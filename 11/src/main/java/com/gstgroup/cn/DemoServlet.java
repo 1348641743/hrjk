10211431a5363828be4a47925f724f7784047104
1348641743 <36215871+1348641743@users.noreply.github.com>
2019-02-19 14:41:12 +0800

    Add files via upload

---
+++ 11/src/main/java/com/gstgroup/cn/DemoServlet.java
@@ -0,0 +1,67 @@
+package com.gstgroup.cn;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String username = request.getParameter("user");
+		String pass = request.getParameter("pass");
+		System.out.println(username);
+		System.out.println(pass);
+		//判断是否正确
+		if(username.equals("admin") && pass.equals("123")){
+//			System.out.println("输入正确");
+//			 response.setCharacterEncoding("UTF-8"); // 先指定输出流的编码
+//			 response.setContentType("text/html;charset=UTF-8");
+//			 response.setCharacterEncoding("UTF-8");
+//			
+//			 
+//			//正确，则进入另一个页面
+//			request.setAttribute( "user",username);
+			HttpSession session=request.getSession();
+			session.setAttribute( "user",username);
+			response.sendRedirect("suc.jsp");
+//			request.getRequestDispatcher("suc.jsp").forward(request, response);
+		}else{
+			//不正确，则返回登录页面
+			HttpSession session=request.getSession();
+			session.setAttribute( "message","账号/密码错误，请重新登录");
+			response.sendRedirect("index.jsp");
+		}
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
---
+++ 11/src/main/webapp/suc.jsp
@@ -0,0 +1,41 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>华软金科-统一登录平台</title>
+
+<style type="text/css">
+body{ text-align:center; vertical-align:middle; } 
+</style>
+
+<style type="text/css">
+.td1{text-align:left; }
+</style>
+
+</head>
+
+<body background="img/1.jpg">
+
+<%
+	String  kk=(String)session.getAttribute("user");
+%>
+ 
+<div style="width:20%; height:100%; background-color:#234; z-index:1;padding-left:0px; ">
+<td  style="padding-left:100px;"><font color="RED">欢迎您登录：</font><font color="#cf0cf9"> <%=kk %></font>  &nbsp; <a href="index.jsp" target="_top" "><font color="#FFFFFFFF">    退出</font></a></td>
+
+</div>
+<tr valign="center"><BR><BR>
+<td align="center"  ><center><font color="blank" size="5" >演示系统列表</font></center></td>
+<br> 
+<table align="center" width="60%" border="0px" bgcolor="#eeeeee" cellpadding="3" >
+<tr bgcolor="#CFFFF0"><td>系统名称</td><td>描述</td></tr>
+<tr ><td align="left"><a href="https://dop.poc.qloudmart.com"  target="frame1">QloudDOP</a></td><td class="td1" colspan='0' style='word-wrap: break-word' width="120"><font size="2">QloudDOP系统，团队可以更快，更高效和更好地交付应用程序.这里有你的计划，监控，开发和协作您的软件项目.</font></td></tr><br>
+<tr><td class="td1" width="20%"><a href="http://qloudtcms.service.sd">QloudTest</a></td><td class="td1"><font size="2">自动化测试平台</font></td></tr>
+<tr><td class="td1"><a href="https://114.115.132.40:8443">Market</a></td><td class="td1"><font size="2">华软金科简融市场体验区</font></td></tr>
+<tr><td class="td1"><a href="">AutoTest</a></td><td class="td1"><font size="2">AutoTest</font></td></tr>
+</table>
+</tr>
+</body>
+</html>
